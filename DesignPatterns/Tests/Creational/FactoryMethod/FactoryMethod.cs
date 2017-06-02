51aa0815b73557dcecd673c2e2ed1a7bcf9a0622
KikeyMeenan <mikemk309@googlemail.com>
2017-06-02 17:31:25 +0100

    started factory method

---
+++ DesignPatterns/Tests/Creational/FactoryMethod/FactoryMethod.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Creational.FactoryMethod
+{
+    public class FactoryMethod
+    {
+        public static void Go()
+        {
+            Document[] documents = new Document[2];
+
+            documents[0] = new Resume();
+            documents[1] = new Report();
+        }
+
+        //http://www.dofactory.com/net/factory-method-design-pattern
+        abstract class Document
+        {
+            private List<string> _names = new List<string>();
+
+            //this is the factory method part!
+            public Document()
+            {
+
+            }
+        }
+    }
+}
