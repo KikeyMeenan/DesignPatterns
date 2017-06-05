bd906ba840beab5fe4cf6223ab1ef48646553edb
KikeyMeenan <mikemk309@googlemail.com>
2017-06-05 09:35:11 +0100

    complete factory method and test

---
+++ DesignPatterns/Tests/Creational/FactoryMethod/CarInstructions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Creational.FactoryMethod
+{
+    public class CarInstructions
+    {
+        public static List<string> GetInstructions(bool isAuto)
+        {
+            InstructionsDocument instructions;
+
+            if (isAuto)
+            {
+                instructions = new AutoInstructions();
+            }
+            else
+            {
+                instructions = new ManualInstructions();
+            }
+
+            return instructions.instructions;
+        }
+
+        //http://www.dofactory.com/net/factory-method-design-pattern
+        abstract class InstructionsDocument
+        {
+            private List<string> _instruction = new List<string>();
+
+            public InstructionsDocument()
+            {
+                this.CreateInstructions();
+            }
+
+            public List<string> instructions
+            {
+                get { return _instruction; }
+            }
+
+            //this is the factory METHOD!!! This pattern is all about the METHOD!
+            public abstract void CreateInstructions();
+        }
+
+        class AutoInstructions : InstructionsDocument
+        {
+            public override void CreateInstructions()
+            {
+                instructions.Add("Turn Key Forward");
+                instructions.Add("Do Nothing");
+                instructions.Add("Turn Key Backward");
+            }
+        }
+
+        class ManualInstructions : InstructionsDocument
+        {
+            public override void CreateInstructions()
+            {
+                instructions.Add("Turn Key Forward");
+                instructions.Add("Put down clutch and move stick");
+                instructions.Add("Turn Key Backward");
+            }
+        }
+    }
+}
---
+++ DesignPatterns/Tests/Creational/FactoryMethod/Tests/CarInstructions_Tests.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace DesignPatterns.Creational.FactoryMethod.Tests
+{
+    public class CarInstructions_Tests
+    {
+        [Fact]
+        public void ReadInstructions_Auto()
+        {
+            var expected = new List<string>()
+            {
+                "Turn Key Forward",
+                "Do Nothing",
+                "Turn Key Backward"
+            };
+            var result = CarInstructions.GetInstructions(true);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void ReadInstructions_Manual()
+        {
+            var expected = new List<string>()
+            {
+                "Turn Key Forward",
+                "Put down clutch and move stick",
+                "Turn Key Backward"
+            };
+            var result = CarInstructions.GetInstructions(false);
+            Assert.Equal(expected, result);
+        }
+    }
+}
